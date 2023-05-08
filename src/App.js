045651feee03ec3111242c786c7cacce5bff9fe3
Edward Azarevich <edward.azarevich@gmail.com>
2023-05-08 19:39:18 +0300

    add new About Us information

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header/Header';
+import CoffeeBeans from './component/CoffeeBeans/CoffeeBeans';
 
 function App() {
+
+    const part1AdoutUs = `
+    Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
+    Afraid at highly months do things on at.Situation recommend objection do intention
+    so questions.As greatly removed calling pleased improve an.Last ask him cold feel
+    met spot shy want.Children me laughing we prospect answered followed.At it went
+    is song that held help face.`;
+
+    const part2AdoutUs = `Now residence dashwoods she excellent you. Shade being under his bed her, Much
+    read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant
+    horrible but confined day end marriage. Eagerness furniture set preserved far
+    recommend. Did even but nor are most gave hope. Secure active living depend son
+    repair day ladies now.`;
     return (
         <div >
             <Header />
+            <div className='about-info' >
+                <i>About Us</i>
+                <CoffeeBeans color={'black'} />
+            </div>
+            <div className='about-us'>
+                <p className='for-parts'>
+                    {part1AdoutUs}
+                </p >
+
+                <p className='for-parts'>
+                    {part2AdoutUs}
+                </p>
+
+            </div>
         </div>
     );
 }
