8a8ac1922d0dc48cbf6aaecce94f52df764a0572
Edward <edward.azarevich@gmail.com>
2024-08-21 20:40:40 +0300

    add new component

---
+++ src/component/pages/1.js
@@ -0,0 +1,8 @@
+let a;
+const b = a;
+
+a = 10;
+
+console.log(a)
+
+console.log(b)
--- src/component/pages/index.js
+++ src/component/pages/index.js
@@ -3,3 +3,8 @@ import MainPages from './MainPages';
 import Pleasure from './Pleasure';
 import Page404 from './404'
 export { MainPages, ForYourPleasure, Pleasure, Page404 }
+
+
+for(var i = 10; i < 35; i += 5) {
+    console.log(i);
+}
